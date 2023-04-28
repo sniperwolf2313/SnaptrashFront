9b77d7a3132d54826dbdc3b93960b79dd6257135
Michael Cardenas <maicol.cardenas2313@gmail.com>
2023-04-27 19:31:17 -0500

    Cambio de versiones de dependencias y se agregaron las vistas principales

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import {Route, Routes } from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Home from './views/Home/Home'
+import Report from './views/Report/Report'
+import Login from './views/Login/Login'
+import SignUp from './views/SignUp/SignUp'
 
 function App() {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path ='/' element={<Home/>}/>
+          <Route path ='/login' element={<Login/>}/>
+          <Route path ='/signup' element={<SignUp/>}/>
+          <Route path ='/report' element={<Report/>}/>
+          <Route path ='/help' element={<Home/>}/>
+        </Routes>
+      </Router>
+    </div>
+
+  );
 }
 
 export default App
